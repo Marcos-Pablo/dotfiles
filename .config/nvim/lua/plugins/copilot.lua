4182a079948bfc6c0718620a66a046dc44fc38a9
Marcos Pablo <marquin.pablo@gmail.com>
2026-07-20 02:16:09 +0200

    chore(nvim): Remove copilot chat and claude integration plugin

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -62,29 +62,4 @@ return {
       require('copilot_cmp').setup()
     end,
   },
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    dependencies = {
-      { 'zbirenbaum/copilot.lua' }, -- or zbirenbaum/copilot.lua
-      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
-    },
-    build = 'make tiktoken', -- Only on MacOS or Linux
-    opts = {
-      -- See Configuration section for options
-      window = {
-        width = 0.4,
-        height = 0.4,
-      },
-    },
-    keys = {
-      { '<leader>zm', ':CopilotChatModels<CR>', mode = 'n', desc = 'Select Models' },
-      { '<leader>zc', ':CopilotChat<CR>', mode = { 'n', 'v' }, desc = 'Chat with Copilot' },
-      { '<leader>ze', ':CopilotChatExplain<CR>', mode = 'v', desc = 'Explain Code' },
-      { '<leader>zr', ':CopilotChatReview<CR>', mode = 'v', desc = 'Review Code' },
-      { '<leader>zf', ':CopilotChatFix<CR>', mode = 'v', desc = 'Fix Code Issues' },
-      { '<leader>zt', ':CopilotChatTests<CR>', mode = 'v', desc = 'Generate Tests' },
-      { '<leader>zg', ':CopilotChatCommit<CR>', mode = 'n', desc = 'Generate Commit Message' },
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
 }
